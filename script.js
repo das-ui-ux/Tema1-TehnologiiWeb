5cf8a4238222f63acbb0e4dcd74dcb81ed797fd6
Alex-Silviu Dan <79323999+das-ui-ux@users.noreply.github.com>
2022-10-17 13:44:55 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById("contactModal");
+var btn = document.getElementById("btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
